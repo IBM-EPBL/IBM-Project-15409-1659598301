1a9decb5622d302905ab1be6fc4025f3fb3f71a9
Keerthi-511 <106374247+Keerthi-511@users.noreply.github.com>
2022-09-23 15:15:23 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM MEMBER 3/Assignment - 1/Coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\KING\Desktop\ASSIGNMENT\TEAM MEMBER 2\Assignment - 1\Coding 1.py
+[5, 6, 7, 8, 9]
+[5, 6, 7, 8, 9, 10]
+[6, 7, 8, 9, 10]
+[6, 7, 8, 9, 10, 20]
+[6, 7, 8, 9, 10, 20]
+[6, 7, 8, 9, 10]
+[10, 9, 8, 7, 6]
+HelloString
+gnirtS
+tri
---
+++ ASSIGNMENT/TEAM MEMBER 3/Assignment - 1/Coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[5,6,7,8,9]
+print(list)
+
+#insertion
+list.insert(5,10)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(20)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Hello")
+str2=("String")
+print(str1+str2)
+
+#reverse of String
+str3="String"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,4)])
+
+
+
+
+
+
+
+
+
+
+
+
