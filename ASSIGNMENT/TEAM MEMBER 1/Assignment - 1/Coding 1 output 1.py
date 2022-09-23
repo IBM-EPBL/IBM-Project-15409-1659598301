a486a4a6f2da9d4e1c0bb4638950dcd9fd30056d
farjana-7 <106387978+farjana-7@users.noreply.github.com>
2022-09-23 14:54:15 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM MEMBER 1/Assignment - 1/Coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\KING\Desktop\ASSIGNMENT\TEAM LEAD\Assignment - 1\Coding 1.py
+[22, 32, 42, 52, 62]
+[22, 32, 42, 52, 62, 80]
+[32, 42, 52, 62, 80]
+[32, 42, 52, 62, 80, 70]
+[32, 42, 52, 62, 70, 80]
+[32, 42, 52, 62, 70]
+[70, 62, 52, 42, 32]
+ConcatenateString
+noitarepO
+pe
---
+++ ASSIGNMENT/TEAM MEMBER 1/Assignment - 1/Coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[22,32,42,52,62]
+print(list)
+
+#insertion
+list.insert(5,80)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(70)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Concatenate")
+str2=("String")
+print(str1+str2)
+
+#reverse of String
+str3="Operation"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,3)])
+
+
+
+
+
+
+
+
+
+
+
+
