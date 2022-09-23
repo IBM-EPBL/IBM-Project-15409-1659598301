5f5d1b0743a1d789214ea0d3c3cb7d9c78fecc0d
Agila <106165005+Agila06@users.noreply.github.com>
2022-09-23 14:27:07 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM LEAD/Assignment - 1/Coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\KING\Desktop\ASSIGNMENT\TEAM LEAD\Assignment - 1\Qsn 1\coding 1.py
+[10, 20, 30, 40, 50]
+[10, 20, 30, 40, 50, 60]
+[20, 30, 40, 50, 60]
+[20, 30, 40, 50, 60, 70]
+[20, 30, 40, 50, 60, 70]
+[20, 30, 40, 50, 60]
+[60, 50, 40, 30, 20]
+PrintHello
+esreveR
+ev
---
+++ ASSIGNMENT/TEAM LEAD/Assignment - 1/Coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[10,20,30,40,50]
+print(list)
+
+#insertion
+list.insert(5,60)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(70)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Print")
+str2=("Hello")
+print(str1+str2)
+
+#reverse of String
+str3="Reverse"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,3)])
+
+
+
+
+
+
+
+
+
+
+
+
